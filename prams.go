4e08dbf8d052529d64841a991a34cee916f66083
bryku <drburnett@mail.com>
2018-12-12 17:27:05 -0800

    Create prams.go

---
+++ prams.go
@@ -0,0 +1,20 @@
+package prams
+
+import("os")
+
+func Get(str string) (string, bool){
+	a := os.Args[1:];
+	argsString := ""
+	argsBool := false
+	for key, _ := range a {
+		if(a[key] == str){
+			argsBool = true
+			if(len(a) > key + 1){
+				argsString = a[key + 1]
+			}else{
+				argsString = ""
+			}
+		}
+	}
+	return argsString, argsBool
+}
